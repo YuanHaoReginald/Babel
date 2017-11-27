aabdb9fd208228284e8a02a96f1c972ad3d9da2f
Yuan Hao <yuanhaoreginald@gmail.com>
2017-11-27 12:17:33 +0800

    fix config and add .gitignore

--- Babel/settings.py
+++ Babel/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Add for vuejs
 STATICFILES_DIRS = [
