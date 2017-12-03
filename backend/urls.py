48871c43f4dd81daa5b38794cf9cdf5dabe292a0
Yuan Jun <thss15_yuanj@163.com>
2017-12-03 19:43:53 +0800

    Modify return value of user funcitons

--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-	url(r'^TranslatorSignUp$', views.TranslatorSignUp , name='TransloterSignUp'),
     url(r'^UserSignUp$', views.UserSignUp, name='UserSignUp'),
-    url(r'^EmployerSignUp$', views.EmployerSignUp , name='EmployerSignUp'),
     url(r'^UserSignIn$', views.UserSignIn, name='UserSignIn'),
     url(r'^GetUserInfo$', views.GetUserInfo, name='GetUserInfo'),
+    url(r'UserModify$', views.UserModify, name='UserModify'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -12,43 +12,8 @@ import json
 
 
 # Create your views here.
-# translator sign up
-def TranslatorSignUp(request):
-    if request.method == 'POST':
 
-        info_dict = json.loads(request.body.decode())
-        new_username = info_dict['username']
-        new_password = info_dict['password']
-        new_email = info_dict['email']
-
-        existedTranslator = Translator.objects.filter(username = new_username)
-
-        # fail to sign up because there exists a user
-        if(len(existedTranslator) >= 1):
-            response_dict = {'id':0}
-        else: # succeed and insert
-            translator = Translator.objects.create(username = new_username, password = new_password, email = new_email, utype='Translator')
-            response_dict = {'id': translator.id}
-        return JsonResponse(response_dict)
-
-# employer sign up
-def EmployerSignUp(request):
-    if request.method == 'POST':
-        info_dict = json.loads(request.body.decode())
-        new_username = info_dict['username']
-        new_password = info_dict['password']
-        new_email = info_dict['email']
-
-        existedEmployer = Employer.objects.filter(username=new_username)
-        # fail to sign up because there exists a user
-        if (len(existedEmployer) >= 1):
-            response_dict = {'id': 0}
-        else:  # succeed and insert
-            employer = Employer.objects.create(username=new_username, password=new_password, email=new_email, utype='Employer')
-            response_dict = {'id': employer.id}
-        return JsonResponse(response_dict)
-
-# a comomn sign up
+# user signup
 def UserSignUp(request):
 
     if request.method == 'POST':
@@ -74,7 +39,7 @@ def UserSignUp(request):
         return JsonResponse(response_dict)
 
 
-# translator login in
+# user login
 def UserSignIn(request):
     if request.method == 'POST':
         info_dict = json.loads(request.body.decode())
@@ -132,11 +97,11 @@ def UserModify(request):
             current_user.wechatNumber = user_wechat
             current_user.save()
             # language
-            Language.objects.create(languageType = user_language, TranslatorId = user_id)
-            #
-            response_dict = {'status':1,"id":user_id,"utype":current_user.utype}
+            Language.objects.create(languageType = user_language, TranslatorId = current_user)
+            
+            response_dict = {'status': True}
         else:
-            response_dict = {'status': 0, "id": user_id}
+            response_dict = {'status': False}
 
         return JsonResponse(response_dict)
 
